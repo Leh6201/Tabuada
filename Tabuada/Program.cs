65200791e1ba7cb04cb4be299e0ee2e133d4479a
NOTE <NOTE@DESKTOP-PDNVRDV>
2023-08-17 18:59:00 -0300

    Add project files.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para a tabuada (0 a 10): ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"A tabuada solicitada foi: {numero}\n");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine($"{numero} x {i} = {resultado}");
+            }
+        }
+    }
+}
